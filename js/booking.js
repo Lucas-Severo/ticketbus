f83fef1a856f992f766b1c443116dc00e166400b
Lucas-Severo <lucas_severo@outlook.com.br>
2020-03-29 19:44:44 -0400

    style: customize the selected seat

--- js/booking.js
+++ js/booking.js
@@ -1,15 +1,28 @@
+
+// redirect to index
+if (!(sessionStorage.getItem("name") && sessionStorage.getItem("origin") && sessionStorage.getItem("destiny")))
+    window.location.assign('index.html');
+
 document.querySelector(".name").innerHTML = "Nome: " + sessionStorage.getItem("name");
 document.querySelector(".origin").innerHTML = "Origem: " + sessionStorage.getItem("origin");
 document.querySelector(".destiny").innerHTML = "Destino: " + sessionStorage.getItem("destiny");
 
 const dateObject = new Date();
 
-document.querySelector(".date").innerHTML = "Data: " + `${dateObject.getDate()}/${dateObject.getMonth()}/${dateObject.getFullYear()}`;
+document.querySelector(".date").innerHTML = "Data: " + 
+    `${dateObject.getDate()}/${dateObject.getMonth()}/${dateObject.getFullYear()}`;
+    
 const seatInfo = document.querySelector(".seat");
-
 const seatElement = document.querySelectorAll(".banco");
+
 seatElement.forEach((element) => {
     element.addEventListener("click", function(){
+        selected = document.querySelector('.selected');
+        
+        if(selected)
+            selected.classList.toggle('selected');
+
+        element.classList.toggle("selected");
         seatInfo.innerHTML = "Banco: " + element.innerHTML;
-    })
+    });
 });
