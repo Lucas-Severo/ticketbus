6dd9165e1f63a0126147e053242e1cb39f4aefda
Lucas-Severo <lucas_severo@outlook.com.br>
2020-03-30 09:31:07 -0400

    feat: added print window

--- js/booking.js
+++ js/booking.js
@@ -3,18 +3,25 @@
 if (!(sessionStorage.getItem("name") && sessionStorage.getItem("origin") && sessionStorage.getItem("destiny")))
     window.location.assign('index.html');
 
+sessionStorage.removeItem('seat');
+
 document.querySelector(".name").innerHTML = "Nome: " + sessionStorage.getItem("name");
 document.querySelector(".origin").innerHTML = "Origem: " + sessionStorage.getItem("origin");
 document.querySelector(".destiny").innerHTML = "Destino: " + sessionStorage.getItem("destiny");
 
+// date
 const dateObject = new Date();
-
-document.querySelector(".date").innerHTML = "Data: " + 
+document.querySelector(".date").innerHTML = "Data da reserva: " + 
     `${dateObject.getDate()}/${dateObject.getMonth()}/${dateObject.getFullYear()}`;
+
+// hour
+document.querySelector(".hour").innerHTML = "Horário: " + 
+    `${dateObject.getHours()}:${dateObject.getMinutes()}:${dateObject.getSeconds()}`;
     
 const seatInfo = document.querySelector(".seat");
 const seatElement = document.querySelectorAll(".banco");
 
+// select seat
 seatElement.forEach((element) => {
     element.addEventListener("click", function(){
         selected = document.querySelector('.selected');
@@ -23,6 +30,18 @@ seatElement.forEach((element) => {
             selected.classList.toggle('selected');
 
         element.classList.toggle("selected");
+        sessionStorage.setItem('seat', element.innerHTML);
         seatInfo.innerHTML = "Banco: " + element.innerHTML;
     });
+});
+
+// print
+document.querySelector(".info-field .submit").addEventListener("click", function(){
+    if(sessionStorage.getItem('seat')) {
+        document.querySelector(".bus").style.display = "none";
+        document.querySelector(".info").classList.toggle("print");
+        window.print();
+        document.querySelector(".bus").style.display = "flex";
+        document.querySelector(".info").classList.toggle("print");
+    }
 });
